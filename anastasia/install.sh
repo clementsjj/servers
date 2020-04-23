d8f1c437d28fb74880dbddd692804c22dad1bef7
JJ Clements <clementsjj@gmail.com>
2020-04-23 19:54:31 -0400

    Update install.sh

--- anastasia/install.sh
+++ anastasia/install.sh
@@ -27,7 +27,7 @@ echo "export PATH=~/.npm-global/bin:$PATH" >> ~/.profile
 #sudo apt-get install sqlite3
 #sudo apt install tree
 #sudo apt install tmux
-echo -e "\033[9;34m sudo apt update '~/.npm-global' \033[m"
+echo -e "\033[9;34m sudo apt update \033[m"
 sudo apt update
 echo -e "\033[9;34m sudo apt install -y nginx \033[m"
 sudo apt install -y nginx
