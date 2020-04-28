91a9204bd19534a8fa44b1d5552367c5441883e2
JJ Clements <clementsjj@gmail.com>
2020-04-28 16:54:48 -0400

    Add mysql db setup to backend.sh

--- bezukhov/scripts/setupbackend.sh
+++ bezukhov/scripts/setupbackend.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-mkdir /home/jj/www/backend
-yarn create strapi-app /home/jj/www/backend --quickstart --no-run
+# Setup MySQL Db
+# sudo mysql -u root
+# > create database strapi;
+# > create user dumbo@localhost identified by 'password';
+# > grant all privileges on strapi.* to dumbo@localhost
+# > flush privileges;
+# > quit;
+
+
+mkdir /home/jj/www/strapi
+#yarn create strapi-app /home/jj/www/backend --quickstart --no-run
 #yarn strapi install graphql
+yarn create strapi-app /home/jj/www/strapi --no-run
+
+
+
+
+
+
+
+
+# start strapi
 yarn develop
